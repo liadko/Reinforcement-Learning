9e6b9a22b3313e977df18c266c5b9064466d421e
liadko <liadkoren@gmail.com>
2024-08-05 12:30:43 +0300

    State Struct

--- RL.cpp
+++ RL.cpp
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <iomanip>
 #include <string>
+#include <queue>
 
 using std::cout;
 using std::vector;
@@ -12,7 +13,6 @@ using std::string;
 const float GAMMA = 0.95f;
 const float EPSILON = 0.1f;
 const float ALPHA = 0.5f;
-
 
 const int M = 10000;
 
@@ -25,42 +25,46 @@ float policy[W][H][4];
 int dx[4] = { 0, 1, 0, -1 };
 int dy[4] = { -1, 0, 1, 0 };
 
+struct State
+{
+	int x, y;
+};
 
 // returns direction to move in, based on the policy and epsilon
-int selectAction(int x, int y, float epsilon)
+int selectAction(const State& state , float epsilon)
 {
 	if ((static_cast<float>(rand()) / RAND_MAX) < epsilon)
 		return rand() % 4;
 
 	int best_a = 0;
 	for (int a = 1; a < 4; a++)
-		if (policy[x][y][a] > policy[x][y][best_a])
+		if (policy[state.x][state.y][a] > policy[state.x][state.y][best_a])
 			best_a = a;
 
 	return best_a;
 }
 
 // returns if game still running
-bool nextState(int x, int y, int a, int& next_x, int& next_y, int& r)
+bool nextState(const State& state, int a, State& next_state, int& r)
 {
-	next_x = x + dx[a];
-	next_y = y + dy[a];
+	next_state = { state.x + dx[a], state.y + dy[a] };
+	
+	// Bounds
+	if (next_state.x < 0) next_state.x = 0;
+	else if (next_state.x >= W) next_state.x = W - 1;
+	if (next_state.y < 0) next_state.y = 0;
+	else if (next_state.y >= H) next_state.y = H - 1;
 
-	if (next_x < 0) next_x = 0;
-	else if (next_x >= W) next_x = W - 1;
-	if (next_y < 0) next_y = 0;
-	else if (next_y >= H) next_y = H - 1;
 
-
-	if (next_y == H - 1)
+	if (next_state.y == H - 1)
 	{
-		if (next_x == W - 1)
+		if (next_state.x == W - 1)
 		{
 			r = 0;
 			return false;
 		}
 
-		else if (next_x > 0)
+		else if (next_state.x > 0)
 		{
 			r = -100;
 			return false;
@@ -79,25 +83,25 @@ float sarsaPrediction(int next_x, int next_y, int next_a)
 }
 
 // consider the best possible next_action.
-float qLearningPrediction(int next_x, int next_y)
+float qLearningPrediction(const State& next_state)
 {
-	return policy[next_x][next_y][selectAction(next_x, next_y, 0)];
+	return policy[next_state.x][next_state.y][selectAction(next_state, 0)];
 }
 
 // weigh the prediction based on the next action probabilities.
-float expectedSarsaPrediction(int next_x, int next_y, float epsilon)
+float expectedSarsaPrediction(const State& next_state, float epsilon)
 {
 	float probs[4];
 	for (int a = 0; a < 4; a++)
 		probs[a] = epsilon / 4.0f;
 
-	int best_action = selectAction(next_x, next_y, 0);
+	int best_action = selectAction(next_state, 0);
 	probs[best_action] += 1 - epsilon;
 
 
 	float weighted_average = 0;
 	for (int a = 0; a < 4; a++)
-		weighted_average += policy[next_x][next_y][a] * probs[a];
+		weighted_average += policy[next_state.x][next_state.y][a] * probs[a];
 	
 	
 	return weighted_average;
@@ -105,8 +109,10 @@ float expectedSarsaPrediction(int next_x, int next_y, float epsilon)
 
 void playGame(float epsilon, bool debug = false)
 {
-	int x = 0, y = 3, a = selectAction(x, y, epsilon);
-	int next_x, next_y, next_a;
+	State state = { 0, 3 }; int a = selectAction(state, epsilon);
+	State next_state; int next_a;
+	
+	
 	int r;
 
 
@@ -115,21 +121,21 @@ void playGame(float epsilon, bool debug = false)
 	while (running)
 	{
 		// get next state
-		running = nextState(x, y, a, next_x, next_y, r);
+		running = nextState(state, a, next_state, r);
 
-		next_a = selectAction(next_x, next_y, epsilon);
+		next_a = selectAction(next_state, epsilon);
 
 
 		if (debug) // print
-			cout << "[" << x << ", " << y << "] a:" << a << " r:" << r << " -> ";
+			cout << "[" << state.x << ", " << state.y << "] a:" << a << " r:" << r << " -> ";
 		else // update state action pair
 		{
-			//float next_q_prediction = sarsaPrediction(next_x, next_y, next_a);
-			//float next_q_prediction = qLearningPrediction(next_x, next_y);
-			float next_q_prediction = expectedSarsaPrediction(next_x, next_y, epsilon);
+			//float next_q_prediction = sarsaPrediction(next_state, next_a);
+			//float next_q_prediction = qLearningPrediction(next_state);
+			float next_q_prediction = expectedSarsaPrediction(next_state, epsilon);
 
 			float expected_r = r + GAMMA * next_q_prediction;
-			policy[x][y][a] += ALPHA * (expected_r - policy[x][y][a]);
+			policy[state.x][state.y][a] += ALPHA * (expected_r - policy[state.x][state.y][a]);
 		}
 		
 		
@@ -137,11 +143,11 @@ void playGame(float epsilon, bool debug = false)
 
 
 		// execute action
-		x = next_x; y = next_y;
+		state = next_state;
 		a = next_a;
 	}
 
-	if (debug) cout << "[" << x << ", " << y << "] -> DONE.\n\n";
+	if (debug) cout << "[" << state.x << ", " << state.y << "] -> DONE.\n\n";
 
 
 
